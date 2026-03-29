d0f9b3dfe048ee4f9b67e82548cb08e38d18535b
Merthasant <MerthaSant28@gmail.com>
2026-03-29 11:53:25 +0800

    menambahkan middleware di user.route

--- backend/src/modules/user/user.route.ts
+++ backend/src/modules/user/user.route.ts
@@ -1,12 +1,38 @@
 import express from "express";
 import userController from "./user.controller.js";
+import accessMiddleware from "@/middlewares/access.middleware.js";
 
 const router = express.Router();
 
-router.get("/", userController.getAllUser);
-router.get("/:id", userController.getOneUser);
-router.post("/", userController.createUser);
-router.patch("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get(
+  "/",
+  accessMiddleware.verifyUser,
+  accessMiddleware.isAdmin,
+  userController.getAllUser,
+);
+router.get(
+  "/:id",
+  accessMiddleware.verifyUser,
+  accessMiddleware.isAdmin,
+  userController.getOneUser,
+);
+router.post(
+  "/",
+  accessMiddleware.verifyUser,
+  accessMiddleware.isAdmin,
+  userController.createUser,
+);
+router.patch(
+  "/:id",
+  accessMiddleware.verifyUser,
+  accessMiddleware.isAdmin,
+  userController.updateUser,
+);
+router.delete(
+  "/:id",
+  accessMiddleware.verifyUser,
+  accessMiddleware.isAdmin,
+  userController.deleteUser,
+);
 
 export default router;
