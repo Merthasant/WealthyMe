1f2413f57c24f26f1dfa75497b1a9f079e5fea25
Merthasant <MerthaSant28@gmail.com>
2026-03-31 07:35:15 +0800

    set column createdAt dan updatedAt memiliki default

---
+++ backend/prisma/migrations/20260330225408_ubah_semua_column_created_at/migration.sql
@@ -0,0 +1,27 @@
+-- AlterTable
+ALTER TABLE "account" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "category" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "profile" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "refreshToken" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "role" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "transaction" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "user" ALTER COLUMN "createdAt" SET DEFAULT 0,
+ALTER COLUMN "updatedAt" SET DEFAULT 0;
