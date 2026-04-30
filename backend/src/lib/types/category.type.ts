b7db1d4124f2c717842e1c2baf2b9c485891f945
Merthasant <MerthaSant28@gmail.com>
2026-04-30 18:57:46 +0800

    menambahkan message di type, agar lebih jelas

--- backend/src/lib/types/category.type.ts
+++ backend/src/lib/types/category.type.ts
@@ -4,7 +4,9 @@ export const createCategorySchema = z.object({
   name: z.string({ message: "name is required" }).min(2, {
     message: "name must be at least 2 characters",
   }),
-  type: z.enum(["income", "expense"], { message: "Invalid category type" }),
+  type: z.enum(["income", "expense"], {
+    message: "category type must be either 'income' or 'expense'",
+  }),
 });
 
 export const updateCategorySchema = createCategorySchema.partial();
