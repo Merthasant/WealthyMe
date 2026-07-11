c5e239a091267fa9a132ccef1ca6a7333e5185ac
Merthasant <Merthasant28@gmail.com>
2026-07-11 19:23:16 +0800

    (fix) memperbaiki main.tsx (keselahan tidak tercontrol dari agents)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,19 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AuthCheck from "./check/auth.check.tsx";
-import ProfileCheck from "./check/profile.check.tsx";
+import AuthChecker from "./checker/auth.checker.tsx";
+import ProfileChecker from "./checker/profile.checker.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthCheck>
-        <ProfileCheck>
+      <AuthChecker>
+        <ProfileChecker>
           <App />
-        </ProfileCheck>
-      </AuthCheck>
+        </ProfileChecker>
+      </AuthChecker>
     </QueryClientProvider>
   </StrictMode>,
 );
