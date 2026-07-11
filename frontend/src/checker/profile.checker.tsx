e2c963aa8b31f096e83bdb821fbdebfd268ac13b
Merthasant <Merthasant28@gmail.com>
2026-07-11 19:22:23 +0800

    membuat profile checker

---
+++ frontend/src/checker/profile.checker.tsx
@@ -0,0 +1,30 @@
+import type { ChildrenProps } from "@/components/organisms/auth/auth.organism.type";
+import { getProfile } from "@/lib/APIs/services/profile.service";
+import { useAuthStore } from "@/store/auth.store";
+import { useProfileStore } from "@/store/profile.store";
+import { useEffect, useState } from "react";
+
+export default function ProfileChecker({ children }: ChildrenProps) {
+  const [isChecking, setIsChecking] = useState<boolean>(true);
+  const accessToken = useAuthStore((s) => s.accessToken);
+  const setHaveProfile = useProfileStore((s) => s.setHaveProfile);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const { data } = await getProfile();
+        if (data) setHaveProfile(true);
+      } catch (err) {
+        console.error("profile checker error", err);
+      }
+    };
+
+    if (accessToken) {
+      init().finally(() => setIsChecking(false));
+    }
+  }, [setHaveProfile, accessToken]);
+
+  if (isChecking) return <h1>Loading...</h1>;
+
+  return <>{children}</>;
+}
