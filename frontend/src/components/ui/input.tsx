3c5314059c3d23c28d5c3043a76d5ac39279aa0f
Merthasant <Merthasant28@gmail.com>
2026-08-04 08:51:26 +0800

    (change) mengubah ui yang tidak konsisten dari dark dengan light theme, input memperbaik ui di type in

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -21,8 +21,8 @@ function Input({
         <label
           htmlFor={id}
           className={cn(
-            "px-2 border-r",
-            iconPosition === "in" ? "absolute *:p-1 *:size-6" : "",
+            "px-2 ",
+            iconPosition === "in" ? "absolute *:p-1 *:size-6" : "border-r",
           )}
         >
           {icon}
@@ -33,7 +33,7 @@ function Input({
         data-slot="input"
         className={cn(
           "h-7 w-full min-w-0 rounded-md border border-input bg-input/20 px-3 py-0.5 text-sm transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-xs/relaxed file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-2 aria-invalid:ring-destructive/20 md:text-xs/relaxed dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
-          icon && iconPosition === "in" ? "pl-12" : "",
+          icon && iconPosition === "in" ? "pl-9" : "",
           className,
         )}
         id={id}
